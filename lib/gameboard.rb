ff24ff6aaeb480e0bca28d5d8824aceb0ed18653
burtlo <franklin.webber@gmail.com>
2011-04-22 09:44:57 -0700

    Gameboard updates

--- lib/gameboard.rb
+++ lib/gameboard.rb
@@ -1,4 +1,44 @@
 
-  class Gameboard
+class Gameboard
 
- end
+  attr_accessor :width, :height, :cells
+
+  #
+  # Generate a gameboard
+  #
+  # @example a board width of 3 and height of 6
+  #
+  #  Gameboard.new :width => 3, :height => 6
+  #
+  def initialize(params = {})
+
+    params.each_pair do |property,value|
+      send("#{property}=",value)
+    end
+
+    computer_units = Creator.generate_computer
+    computer_units = Creator.generate_computer
+
+    middleground = []
+    6.times {|ground| middleground << nil }
+
+    player_units = Creator.generate_player
+
+    battlefield = computer_units + middleground + player_units
+
+    index = 0
+
+    width.times do |x|
+      height.times do |y|
+        @cells << Cell.new(x,y,battlefield[index])
+        index += 1
+      end
+    end
+
+  end
+
+
+
+
+
+end
--- lib/stratego.rb
+++ lib/stratego.rb
@@ -1,4 +1,4 @@
 require File.dirname(__FILE__) + "/unit"
 require File.dirname(__FILE__) + "/creator"
-require File.dirname(__FILE__) + "/gameboard"
 require File.dirname(__FILE__) + "/cell"
+require File.dirname(__FILE__) + "/gameboard"
